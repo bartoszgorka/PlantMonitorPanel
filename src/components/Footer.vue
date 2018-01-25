b876d79ea04503b731651b0e3d289278360469c2
Bartosz Górka <bartosz.gorka@appunite.com>
2018-01-25 23:51:11 +0100

    Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="c-footer__footer">
+    <p class="u-textCenter u-textMuted u-fontSize-12">Bartosz Górka © 2018<br>All right reserved.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'footer-template'
+  }
+</script>
